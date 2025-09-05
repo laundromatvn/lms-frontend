3228c986c08fd85a0ed4059efd3be130478f919d
unibna <nguyenduylan711@gmail.com>
2025-09-06 02:20:57 +0700

    add solar icon

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import { Flex, Typography } from 'antd';
+
+import { CrownMinimalistic } from '@solar-icons/react'
 
 const HomePage: React.FC = () => {
   return (
     <Flex justify="center" align="center" style={{ height: '100vh', width: '100vw' }}>
-      <Typography.Title level={1}>Home Page</Typography.Title>
+      <Typography.Title level={1}>
+        <CrownMinimalistic /> Laundromat
+      </Typography.Title>
     </Flex>
   );
 };
