2715343e5fdc73f1e52f6f8468f7c88646b6dcd5
unibna <nguyenduylan711@gmail.com>
2025-10-09 10:42:21 +0700

    clean up data after accessing home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { Button } from 'antd';
 
 import { useTheme } from '@shared/theme/useTheme';
+
+import { oneTimeTokenStorage } from '@core/storage/oneTimeTokenStorage';
+import { orderStorage } from '@core/storage/orderStorage';
+import { paymentStorage } from '@core/storage/paymentStorage';
+import { selectMachineStorage } from '@core/storage/selectMachineStorage';
+import { storeStorage } from '@core/storage/storeStorage';
+import { tenantStorage } from '@core/storage/tenantStorage';
+import { tokenManager } from '@core/auth/tokenManager';
+import { tokenStorage } from '@core/storage/tokenStorage';
+import { userStorage } from '@core/storage/userStorage';
 
 import { Logo } from '@shared/components/common/Logo';
 import { CenteredLayout } from '@shared/components/layouts/CenteredLayout';
@@ -14,6 +24,18 @@ export const HomePage: React.FC = () => {
   const { t } = useTranslation();
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    oneTimeTokenStorage.clear();
+    orderStorage.clear();
+    paymentStorage.clear();
+    selectMachineStorage.clear();
+    storeStorage.clear();
+    tenantStorage.clear();
+    tokenManager.clear();
+    tokenStorage.clear();
+    userStorage.clear();
+  }, []);
 
   return (
     <CenteredLayout>
