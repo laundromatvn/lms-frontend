6c78194f7ced23d4ff3e4b24a5f8443137e36403
unibna <nguyenduylan711@gmail.com>
2025-10-19 19:06:14 +0700

    ensure page load once when request triggered

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,8 @@ import { createRoot } from 'react-dom/client'
 import 'antd/dist/reset.css';
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from './shared/theme/ThemeProvider.tsx';
 import '@shared/services/i18n';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <App />,
 )
