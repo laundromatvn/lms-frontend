d209d1a27c764b028518b37f254a5e675b182aeb
unibna <nguyenduylan711@gmail.com>
2025-10-20 14:07:26 +0700

    remove redundant

--- src/pages/CustomerFlow/CustomerSelectMachinePage/MachineOptionV2.tsx
+++ src/pages/CustomerFlow/CustomerSelectMachinePage/MachineOptionV2.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import {
-  WashingMachineMinimalistic,
   Waterdrop,
   Wind,
 } from '@solar-icons/react';
